bdbbf92861e4baa6a48f81f257f94767b91ff569
Collin <cwoodr4@students.towson.edu>
2018-02-14 12:15:56 -0500

    Refactored playAction by changing the method signature

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -21,7 +21,7 @@ public abstract class Cell {
 		return available;
 	}
 	
-	public abstract void playAction();
+	public abstract boolean playAction(String msg);
 
 	public void setAvailable(boolean available) {
 		this.available = available;
